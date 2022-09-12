4239c5fd678deed4163cb110f44f7f06bedbe964
pappi-vividmind <84896337+pappi-vividmind@users.noreply.github.com>
2022-09-12 14:27:53 +0200

    Update versions-develop.gradle.kts

--- versions-develop.gradle.kts
+++ versions-develop.gradle.kts
@@ -1,27 +1,29 @@
-ext {
-    toolVersions = [
-            kotlin    : '1.7.0',
-            compileSdk: 32,
-            targetSdk : 32,
-            minSdk    : 21,
-            buildTools: '32.0.0'
-    ]
-    coreLibVersions = [   
-            navigation              : '2.5.1',
-            compose                 : '1.2.0',
-            toothpick               : '3.1.0',
-            toothpickSmoothie       : '3.1.0',
-            retrofit                : '2.9.0',
-            threeten                : '1.4.0',
-            fragment                : '1.5.1',
-            lifecycle               : '2.5.0-beta01',
-            okhttp                  : '5.0.0-alpha.3',
-            rxActivityResult        : '2.2.1',
-            room                    : '2.4.2' 
-    ]
-    testingLibVersions = [
-            mockk                   : '1.12.3',
-            espresso                : '3.4.0',
-            robolectric             : '4.8.1'
-    ]
+object ToolVersions(
+    const val kotlin = "1.7.0",
+    const val compileSdk = 32,
+    const val targetSdk = 32,
+    const val minSdk = 21,
+    const val naploMinSdk = 24,
+    const val buildTools = "32.0.0"
+)
+
+object CoreLibVersions(
+    const val navigation = "2.5.1",
+    const val compose = "1.2.1",
+    const val toothpick = "3.1.0",
+    const val toothpickSmoothie = "3.1.0",
+    const val retrofit = "2.9.0",
+    const val threeten = "1.4.0",
+    const val fragment = "1.5.2",
+    const val lifecycle = "2.5.1",
+    const val okhttp = "5.0.0-alpha.7",
+    const val rxActivityResult = "2.2.1",
+    const val room = "2.4.3",
+    const val firestore = "24.2.2"
+)
+
+object TestingLibVersions(
+    const val mockk = "1.12.3",
+    const val espresso = "3.4.0",
+    const val robolectric = "4.8.1"
 }
